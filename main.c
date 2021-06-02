596c3015a1bf1ada6a1c58ac7b65e1625b464e44
yousifsamir <yousifysm@gmail.com>
2021-06-03 00:55:57 +0200

    use all func in main

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "init.h"
 #include "timer.h"
 #include "LCD.h"
+#include "led.h"
 #include "dataTransformation.h"
 
 
@@ -15,9 +16,18 @@
 int main(void)
 {
 	//init();
+	PortA_Init();
+	PortB_Init();
+	PortF_Init();
+	lcd_init();
+	uint16_t x =0;
 	
-
 	while(1){//main loop
+		
+		lcd_intout(x);
+		x+=10;
+		green_led_on_off(x);
+		delay(500);  // 500ms
 	
 	}
 	
