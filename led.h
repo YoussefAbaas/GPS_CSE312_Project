98e39b160956efc86e8c4f461ca1832d4cd7baf8
fadyNabil16 <68451496+fadyNabil16@users.noreply.github.com>
2021-06-02 23:46:12 +0200

    Update led.h

--- led.h
+++ led.h
@@ -8,7 +8,7 @@ void green_led_on_off(double distance);
 
 void green_led_on_off(double distance)
 {
-    if (distance >= 100) // if distanc 99.8 or 100.3 isequal to 100
+    if (distance >= 100) 
     {
         delay(10);
         GPIO_PORT_DATA_R = 0x08;
